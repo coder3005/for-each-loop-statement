90c549cd4e8446c9405c2b639f1d634304154e94
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-18 17:16:00 +0530

    Create ForEach1.java

---
+++ ForEach1.java
@@ -0,0 +1,13 @@
+/* for-each Loop Statement..! */
+class ForEach
+{
+    public static void main(String[] args) {
+
+        int a[]={10,20,30,40,50};
+
+        for ( int b : a) {
+            
+            System.out.print(b + " ");
+        }
+    }
+}
